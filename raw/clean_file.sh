3dbcec1391ee661ddd896f75207a87a91db889be
mdekauwe <mdekauwe@gmail.com>
2019-11-14 12:32:03 +1100

    Fixed issue in data cleaning

--- raw/clean_file.sh
+++ raw/clean_file.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gawk '$1=$1' Loobos_1997.dat > tmp; mv tmp Loobos_1997.dat
+#gawk '$1=$1' Loobos_1997.dat > tmp; mv tmp Loobos_1997.dat
+sed 's/  \+/ /g' Loobos_1997.dat > tmp; mv tmp Loobos_1997.dat
